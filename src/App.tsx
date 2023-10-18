72e8975eff0af17d632ec31696adb0a10ca706fe
ndutagrace25 <ndutagrace25@gmail.com>
2023-10-18 12:51:15 +0300

    Implement searching

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  search: string;
 }
 
 const App = () => {
@@ -28,7 +29,9 @@ const App = () => {
       }}
     >
       <GridItem area="nav">
-        <NavBar />
+        <NavBar
+          onSearch={(search: string) => setGameQuery({ ...gameQuery, search })}
+        />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,11 +1,17 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { BsSearch } from "react-icons/bs";
+import { SearchProps } from "./NavBar";
 
-const SearchInput = () => {
+const SearchInput = ({ onSearch }: SearchProps) => {
   return (
     <InputGroup>
       <InputLeftElement children={<BsSearch />} />
-      <Input borderRadius={20} placeholder="Search games..." variant="filled" />
+      <Input
+        borderRadius={20}
+        placeholder="Search games..."
+        variant="filled"
+        onKeyUp={(e: any) => e.key === "Enter" && onSearch(e.target.value)}
+      />
     </InputGroup>
   );
 };
